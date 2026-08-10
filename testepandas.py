a218e8e52e803617d8fc57a5b1227cc2e4b4acef
Henrique Molinari <henrique.molinari@sou.unifeob.edu.br>
2026-08-10 20:38:23 -0300

    Adicionando algumas formas para visualizar dados

--- testepandas.py
+++ testepandas.py
@@ -4,13 +4,22 @@ import pandas as pd
 
 # Criando um DataFrame
 data = {
-    'Nome': ['Ana', 'Carlos', 'Maria', 'João'],
-    'Idade': [25, 30, 28, 22],
-    'Cidade': ['SP', 'RJ', 'BH', 'POA']
+    "Nome": ["Ana", "Carlos", "Maria", "João"],
+    "Idade": [25, 30, 28, 22],
+    "Cidade": ["SP", "RJ", "BH", "POA"],
 }
 
 df = pd.DataFrame(data)
-print(df)
+# print(df)
 
 # Primeiras linhas
 print(df.head(2))
+
+# Últimas linhas
+print(df.tail(2))
+
+# Informações do DataFrame
+print(df.info())
+
+# Estatísticas descritivas
+print(df.describe())
