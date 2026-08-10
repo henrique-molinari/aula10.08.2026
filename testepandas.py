d72cb79e2d2d30f85b2200dc39f67ca6222bcdea
Henrique Molinari <henrique.molinari@sou.unifeob.edu.br>
2026-08-10 20:45:11 -0300

    Removendo uma coluna

--- testepandas.py
+++ testepandas.py
@@ -12,19 +12,23 @@ data = {
 df = pd.DataFrame(data)
 # print(df)
 
+# # Selecionar uma coluna
+# print(df["Nome"])
+
+# # Selecionar múltiplas colunas
+# print(df[["Nome", "Idade"]])
+
+# # Selecionar linhas por índice
+# print(df.iloc[1])  # Segunda linha
+# print(df.iloc[1:3])  # Linhas 2 e 3
+
+# # Selecionar por condição
+# print(df[df['Idade'] > 25])
+
 # Adicionar nova coluna
 df['Salário'] = [5000, 6000, 5500, 4500]
 print(df)
 
-# Selecionar uma coluna
-print(df["Nome"])
-
-# Selecionar múltiplas colunas
-print(df[["Nome", "Idade"]])
-
-# Selecionar linhas por índice
-print(df.iloc[1])  # Segunda linha
-print(df.iloc[1:3])  # Linhas 2 e 3
-
-# Selecionar por condição
-print(df[df['Idade'] > 25])
+# Remover coluna
+df = df.drop('Salário', axis=1)
+print(df)
